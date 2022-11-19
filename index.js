e29563562592f2e46338d5115af5a5c20da9b85d
guantai <jpguantai@gmail.com>
2022-11-19 17:21:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+function  moveDodgerLeft(){
+    dodger.style.backgroundColor = "#FF69B4";
+    dodger.style.bottom = "0px";
+    dodger.style.left = "0px";
+}
+
+
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
